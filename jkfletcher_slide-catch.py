d9e4cb95fdc3fd9c41f227fc3e64595202470b4f
jkfletcher98 <jkfletcher@bsu.edu>
2024-11-01 13:41:53 -0400

    Add files via upload

--- jkfletcher_slide-catch.py
+++ jkfletcher_slide-catch.py
@@ -3,3 +3,57 @@
 
 import random, pygame, simpleGE
 
+class Game(simpleGE.Scene):
+    def __init__(self):
+        super().__init__()
+        self.setImage("OGA-Background-1.png")
+        
+        self.rocket = Rocket(self)
+        self.meteor = Meteor(self)
+        
+        self.sprites = [self.rocket, self.meteor]
+
+    def process(self):
+        if self.meteor.collidesWith(self.rocket):
+            self.meteor.reset()
+
+        
+class Rocket(simpleGE.Sprite):
+    def __init__(self, scene):
+        super().__init__(scene)
+        self.setImage("ship_recolor_001.png")
+        self.setSize(75, 75)
+        self.position = (320, 420)
+        self.moveSpeed = 5
+        
+    def process(self):
+        if self.isKeyPressed(pygame.K_LEFT):
+            self.x -= self.moveSpeed
+        if self.isKeyPressed(pygame.K_RIGHT):
+            self.x += self.moveSpeed
+
+
+class Meteor(simpleGE.Sprite):
+    def __init__(self, scene):
+        super().__init__(scene)
+        self.setImage("Asteroid2.png")
+        self.setSize(50, 50)
+        self.reset()
+        
+    def reset(self):
+        self.y = 10
+        self.x = random.randint(0, self.screenWidth)
+        self.dy = random.randint(3, 8)
+
+    def checkBounds(self):
+        if self.bottom > self.screenHeight:
+            self.reset()
+            
+            
+def main():
+    game = Game()
+    game.start()
+    
+    
+if __name__ == "__main__":
+    main()
